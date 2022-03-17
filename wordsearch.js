764f1b9cd09d501227456ebd7db60c3b22a28374
Calvin Chung <calvin-chung@live.com>
2022-03-16 22:54:55 -0700

    pair programming with GraceWXT

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let returnValue = false;
+  if (letters.length && letters[0].length) {
+    const horizontalJoin = letters.map(row => row.join(''));
+    for (let row of horizontalJoin) {
+      if (row.includes(word)) {
+        returnValue = true;
+      }
     }
-}
-
-module.exports = wordSearch
+    const vertJoin = [];
+    let vertWord = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      for (let row of letters) {
+        vertWord += row[i];
+      }
+      vertJoin.push(vertWord);
+    }
+    for (let vertWord of vertJoin) {
+      if (vertWord.includes(word)) {
+        returnValue = true;
+      }
+    }
+  }
+  return returnValue;
+};
+module.exports = wordSearch;
+  
+// in partner with @GraceWXT
